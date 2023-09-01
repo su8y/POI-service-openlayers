c7b1c253a2a2a3c084811145dd74d893fbf3db9f
SUBY <bsa0530@gmail.com>
2023-09-01 22:29:53 +0900

    edit: Update readme & package & config file
    * README : DB 사용법 추가
    * gradle : Querydsl Build Script 작성
    * package.json : client 의존성 버전 변경
    * docker-compose : redis 추가

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 PostGresql &amp; spring boot &amp; postgis
 
+## Info 
+
+* service 
+  * 네이버 API를 이용한 경로 찾기 및 찾은 경로 저장 
+  * 네이버 거리뷰 API를 이용한 POI데이터 거리뷰 
+  * 더미 POI 데이터를 활용한 지도에서 POI데이터 조회하기
+  * 사용자가 POI데이터를 추가가 가능함.
+
+
 ## Install
 
 ----
@@ -38,5 +47,30 @@ docker pull adoptopenjdk/openjdk11:latest
    $ psql -Upostgres project
    # work something..
    ```
- 
+6. Postgresql DummyData setting
+```sql
+update poi set coordinates = st_setsrid(st_makepoint(poi.lon,poi.lat),4326); 
+```
+6. npm install 
+```shell
+$ cd src/main/resources/static
+$ npm install
+```
+7. run gradle `boot jar ` or `complieJava`
 
+8. create `security-env.yml`
+```yaml
+naver:
+   application-name: poi-project
+   secret:
+      key: ${your secret key} # HTTP HEADER: X-NCP-APIGW-API-KEY
+      header: X-NCP-APIGW-API-KEY
+   client:
+      id: ${your client id}
+      header: X-NCP-APIGW-API-KEY-ID
+   api:
+      path:
+         direction5: https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving
+         geocoding: https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode
+         reverse-geocoding: https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc
+```
--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,66 +1,66 @@
 create table MEMBER(
- id VARCHAR(50) primary key,
- password VARCHAR(50) not null,
- email VARCHAR(50) unique,
- role BOOLEAN default false
+                       id VARCHAR primary key,
+                       password VARCHAR not null,
+                       email VARCHAR unique,
+                       role VARCHAR default false
 );
 CREATE TABLE poi (
-	id serial4 NOT NULL,
-	"name" varchar(50) NOT NULL,
-	tel_no varchar(15) NULL,
-	category_code int4 NULL,
-	coordinates public.geometry NULL,
-	description varchar(500) NULL,
-	member_id varchar(50) NULL,
-	lon float4 NULL,
-	lat float4 NULL,
-	CONSTRAINT poi_pkey PRIMARY KEY (id),
-	CONSTRAINT fk_category_id FOREIGN KEY (category_code) REFERENCES public.category(category_code),
-	CONSTRAINT fk_member_id FOREIGN KEY (member_id) REFERENCES public."member"(id) ON DELETE CASCADE ON UPDATE CASCADE
+                     id serial4 NOT NULL,
+                     "name" varchar NOT NULL,
+                     tel_no varchar NULL,
+                     category_code int4 NULL,
+                     coordinates public.geometry NULL,
+                     description varchar(500) NULL,
+                     member_id varchar(50) NULL,
+                     lon float4 NULL,
+                     lat float4 NULL,
+                     CONSTRAINT poi_pkey PRIMARY KEY (id),
+                     CONSTRAINT fk_category_id FOREIGN KEY (category_code) REFERENCES public.category(category_code),
+                     CONSTRAINT fk_member_id FOREIGN KEY (member_id) REFERENCES public."member"(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 create table route(
-	id SERIAL primary key,
-	member_id VARCHAR(50) not null,
-	start_position geometry not null,
-	end_position geometry not null,
-	description VARCHAR(200),
-	-- to be : make table information 
-	constraint fk_member_id foreign key(member_id) references MEMBER(id)
-	on delete cascade
-	on update cascade
+                      id SERIAL primary key,
+                      member_id VARCHAR not null,
+                      title VARCHAR not null,
+                      start_position geometry not null,
+                      end_position geometry not null,
+                      description VARCHAR,
+                      waypoints geometry,
+                      json text,
+                      CONSTRAINT fk_member_id FOREIGN KEY (member_id) REFERENCES public."member"(id)
 );
 create table waypoint(
-	id SERIAL primary key,
-	route_id INTEGER not null,
-	point geometry not null,
-	constraint fk_route_id foreign key(route_id) references route(id)
-	on delete cascade 
-	on update cascade 
+                         id SERIAL primary key,
+                         route_id INTEGER not null,
+                         point geometry not null,
+                         constraint fk_route_id foreign key(route_id) references route(id)
+                             on delete cascade
+                             on update cascade
 );
 CREATE TABLE public.category (
-	category_code int4 NOT NULL,
-	lclascd int4 NULL,
-	lclasdc varchar NULL,
-	mlsfccd int4 NULL,
-	mlsfcdc varchar NULL,
-	sclascd int4 NULL,
-	sclasdc varchar NULL,
-	dclascd int4 NULL,
-	dclasdc varchar NULL,
-	bclascd int4 NULL,
-	bclasdc varchar NULL,
-	rm varchar(50) NULL,
-	CONSTRAINT category_pkey PRIMARY KEY (category_code)
+                                 category_code int4 NOT NULL,
+                                 lclascd int4 NULL,
+                                 lclasdc varchar NULL,
+                                 mlsfccd int4 NULL,
+                                 mlsfcdc varchar NULL,
+                                 sclascd int4 NULL,
+                                 sclasdc varchar NULL,
+                                 dclascd int4 NULL,
+                                 dclasdc varchar NULL,
+                                 bclascd int4 NULL,
+                                 bclasdc varchar NULL,
+                                 rm varchar(50) NULL,
+                                 CONSTRAINT category_pkey PRIMARY KEY (category_code)
 );
 alter table POI add constraint fk_category_id foreign key(category_id)
-references category(category_code);
+    references category(category_code);
 
 create table upload_image(
-	store_file_name varchar,
-	original_file_name varchar not null,
-	ext varchar(10) not null,
-	create_at timestamp,
-	target_id integer not null,
-	type varchar(5) not null,
-	constraint fk_upload_image_id primary key(store_file_name)
+                             store_filename varchar,
+                             original_filename varchar not null,
+                             ext varchar(10) not null,
+                             create_at timestamp,
+                             target_id integer not null,
+                             type varchar(5) not null,
+                             constraint fk_upload_image_id primary key(store_file_name)
 )
